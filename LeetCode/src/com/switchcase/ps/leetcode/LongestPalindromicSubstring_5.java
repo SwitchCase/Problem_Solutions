32437070390cf3388285eab63185f3cf7bbdb823
Ashish Agarwal <ash.agrwal@gmail.com>
2019-11-17 18:02:16 -0800

    Solution for 5.

--- LeetCode/src/com/switchcase/ps/leetcode/LongestPalindromicSubstring_5.java
+++ LeetCode/src/com/switchcase/ps/leetcode/LongestPalindromicSubstring_5.java
@@ -9,26 +9,59 @@ public class LongestPalindromicSubstring_5 {
         public String longestPalindrome(String s) {
             String longest = "";
             for (int i = 0; i < s.length(); i++) {
-                for (int j = 1; j <= s.length(); j++) {
-                    String check = s.substring(i, i + j);
-                    if (isPalindrome(check)) {
-                        if (longest.length() < check.length()) {
-                            longest = check;
-                        }
-                    }
-                }
+                String tmp = findLongest(s, i);
+                longest = longest.length() < tmp.length() ? tmp : longest;
             }
             return longest;
         }
-        boolean isPalindrome(String s) {
-            int left = 0, right = s.length() - 1;
-            while (left < right) {
-                if (s.charAt(left) == s.charAt(right)) {
-                    left++;
-                    right--;
-                } else return false;
+
+        private String findLongest(String s, int pos) {
+            String longest = "" + s.charAt(pos);
+            int n = s.length();
+            //odd length:
+            int left = pos - 1;
+            int right = pos + 1;
+            String tmp = longestPalidrome(s, left, right);
+            longest = longest.length() < tmp.length() ? tmp : longest;
+
+            //leftwards even:
+            left = pos -1;
+            if (left >=0 && s.charAt(left) == s.charAt(pos)) {
+                left--;
+                right = pos + 1;
+                tmp = longestPalidrome(s, left, right);
+                longest = longest.length() < tmp.length() ? tmp : longest;
             }
-            return true;
+
+            //rightwards even:
+            right = pos + 1;
+            if (right < n && s.charAt(right) == s.charAt(pos)) {
+                right++;
+                left = pos - 1;
+                tmp = longestPalidrome(s, left, right);
+                longest = longest.length() < tmp.length() ? tmp : longest;
+            }
+            return longest;
         }
+
+        private String longestPalidrome(String s, int left, int right) {
+            String longest = "";
+            int n = s.length();
+            while (left < right && left >=0 && right < n) {
+                if (s.charAt(left) == s.charAt(right)) {
+                    left--;
+                    right++;
+                } else break;
+            }
+            if (left+1 < n && left + 1 >=0 && right <= n)
+                longest = s.substring(left+1, right);
+
+            return longest;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.longestPalindrome("babadaba"));
     }
 }
