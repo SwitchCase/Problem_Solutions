ab3c74998fb9d9f8615ef7be7bb41c6ce627f489
Ashish Agarwal <ashish.agarwal@walmart.com>
2023-10-24 21:55:21 -0700

    Solution to Populate next right pointer

--- LeetCode/src/com/switchcase/ps/leetcode/PopulateNextRightPointer_116.java
+++ LeetCode/src/com/switchcase/ps/leetcode/PopulateNextRightPointer_116.java
@@ -24,7 +24,87 @@ public class PopulateNextRightPointer_116 {
         }
     }
 
+
     class Solution {
+        public Node connect(Node root) {
+            if (root == null) {
+                return null;
+            }
+            solve(root, null, true);
+            return root;
+        }
+
+        Node solvePre(Node root, Node parent, boolean rootIsLeft) {
+            if (root == null) return root;
+            if (parent != null) {
+                if (!rootIsLeft) {
+                    Node uncle = parent.next;
+                    while (root.next == null && uncle != null) {
+                        root.next = uncle.left != null ? uncle.left : uncle.right;
+                        uncle = uncle.next;
+                    }
+                }
+                else {
+                    root.next = parent.right;
+                    Node uncle = parent.next;
+                    while (root.next == null && uncle != null) {
+                        root.next = uncle.left != null ? uncle.left : uncle.right;
+                        uncle = uncle.next;
+                    }
+                }
+            }
+            solvePre(root.right, root, false);
+            solvePre(root.left, root, true);
+            return root;
+        }
+
+        Node solve(Node root, Node parent, boolean rootIsLeft) {
+            if (root == null) return root;
+            if (root.next != null) return root;
+            if (parent != null) {
+                Node next = null;
+                Node uncle = null;
+                boolean nextIsLeft = false;
+                if (rootIsLeft && parent.right != null) {
+                    next = parent.right;
+                    uncle = parent;
+                } else {
+                    uncle = parent.next;
+                    while (next == null && uncle != null) {
+                        if (uncle.left != null) {
+                            next = uncle.left;
+                            nextIsLeft = true;
+                        } else if (uncle.right != null) {
+                            next = uncle.right;
+                            nextIsLeft = false;
+                        }
+                        uncle = uncle.next;
+                    }
+                }
+                root.next = next;
+                solve(next, uncle, nextIsLeft);
+            }
+            solve(root.left, root, true);
+            solve(root.right, root, false);
+            return root;
+        }
+
+        private Node getNextForRightNode(Node parent) {
+            if (parent == null) {
+                return null;
+            }
+            if (parent.next != null) {
+                if (parent.next.left != null) {
+                    return parent.next.left;
+                } else {
+                    return parent.next.right;
+                }
+            }
+            return null;
+        }
+    }
+
+    class SolutionBFS {
         public Node connect(Node root) {
             if (root == null) {
                 return null;
--- LeetCode/tst/com/switchcase/ps/leetcode/PopulateNextRightPointer_116Test.groovy
+++ LeetCode/tst/com/switchcase/ps/leetcode/PopulateNextRightPointer_116Test.groovy
@@ -17,6 +17,46 @@ class PopulateNextRightPointer_116Test extends Specification {
 
     }
 
+    def test2() {
+        given:
+        PopulateNextRightPointer_116.Solution solution = new PopulateNextRightPointer_116.Solution()
+        PopulateNextRightPointer_116.Node root = build([0,1,2,3, null, 5, 6,7,8,null, null, null, null, 13, 14], 0);
+
+        when:
+        def result = solution.connect(root)
+
+        then:
+        result.left.left.left.next.val == 8
+        println(print(result, ""))
+
+    }
+
+    def test3() {
+        given:
+        PopulateNextRightPointer_116.Solution solution = new PopulateNextRightPointer_116.Solution()
+        PopulateNextRightPointer_116.Node root = build([1,2,3,4,5,null,6,7,null,null,null,null,null,8], 0);
+
+        when:
+        def result = solution.connect(root)
+
+        then:
+        //result.left.left.left.next.val == 8
+        println(print(result, ""))
+
+    }
+
+    String print(PopulateNextRightPointer_116.Node node, String soFar) {
+        if (node == null) return soFar;
+        soFar += node.val + " -> "
+        def rec = node.left == null ? node.right : node.left
+        while(node.next != null) {
+            node = node.next;
+            soFar +=  node.val + " -> "
+        }
+        soFar += "\n"
+        return print(rec, soFar);
+    }
+
     PopulateNextRightPointer_116.Node build(List<Integer> integers, int pos) {
         if (integers.get(pos) == null) {
             return null;
